fa74ff0eb799fb01eb19d03ef526c231704e1428
Nate-River56 <hydrangea56@me.com>
2017-02-27 05:32:05 +0900

    Add callback function

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var hub2cw = require('./lib/hub2cw')
 
 exports.handler = function(event, context){
   console.log('Received event:', JSON.stringify(event, null, 2));
-  hub2cw.run(event, context);
-  context.done();
+  hub2cw.run(event, context, function(){
+    context.done();
+  });
 }
