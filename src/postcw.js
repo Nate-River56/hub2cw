9802e8cbb329cddc6f62e6169fa1e6dbbce99481
Nate-River56 <hydrangea56@me.com>
2017-02-27 05:35:08 +0900

    Fix a lot of code in postcw.js

--- src/postcw.js
+++ src/postcw.js
@@ -1,26 +1,21 @@
 import request from 'superagent';
 
-export default cw;
-
-cw.message = (api_token, room_id, msg) => {
-  return new Promise((resolve, reject) => {
-    request
-      .post(endpoint(room_id))
-      .set('X-ChatWorkToken', api_token)
-      .send(`body=${encodeURIComponent(msg)}`)
-      .end((err, res) => {
-        if (!err && res.statusCode == 200){
-          resolve(res);
-        } else {
-          console.log(res);
-          console.log(err);
-          reject(err);
-        }
-      })
-  })
+exports.message = (api_token, room_id, msg) => {
+  request
+    .post(endpoint(room_id))
+    .set('X-ChatWorkToken', api_token)
+    .send(`body=${encodeURIComponent(msg)}`)
+    .end((err, res) => {
+      if (!err && res.statusCode == 200){
+        console.log("Post Success!!");
+      } else {
+        console.log("Post Failed");
+        console.log(err);
+      }
+    })
 }
 
 const endpoint = (room_id) => {
-  return `https://api.chatwork.com/v2/rooms/${roomid}/messages`
+  return `https://api.chatwork.com/v2/rooms/${room_id}/messages`
 }
 
