a317ff653d71a01ea920b7169a7255371a9b3ff9
Nate-River56 <hydrangea56@me.com>
2017-02-27 05:35:50 +0900

    Embeded handlebars template

--- src/builder.js
+++ src/builder.js
@@ -1,14 +1,33 @@
 import fs from 'fs';
 import handlebars from 'handlebars';
 
-export default builder;
+exports.push = (payload) => {
+  // const source = fs.readFileSync(
+  //   '../templates/hub_push.hbs',
+  //   'utf-8'
+  //   );
+  // console.log(source);
+  
+  const source = `-- Push event --
+CompareURL: {{compare}}
+{{#commits}}
+[info][title]{{message}}[/title]URL: {{url}}
+Commiter: {{committer.username}} ({{committer.name}})
+[hr]{{#if added}}Added:[code]{{#added}}{{this}}
+{{/added}}[/code]
+{{/if}}{{#if modified}}Modified:[code]{{#modified}}{{this}}
+{{/modified}}[/code]
+{{/if}}{{#if removed}}Removed:[code]{{#removed}}{{this}}
+{{/removed}}[/code]
+{{/if}}[/info]{{/commits}}
+`;
 
-builder.push = (payload) => {
-  let template = Handlebars.compile(
-    fs.readFileSync(
-    '../templates/hub_push.hbs',
-    'utf-8'
-    )
-  );
-  return template(payload);
+  const template = handlebars.compile(source);
+
+  const message = template(payload);
+  console.log(message);
+
+  // var message = "Hello, world!!";
+
+  return message;
 }
