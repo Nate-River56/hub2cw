f169e31bb78f1cbf2179cd7f2a3172a04fd9cc87
Nate-River56 <hydrangea56@me.com>
2017-02-26 22:45:15 +0900

    Add source which post to chatwork with message.

---
+++ src/postcw.js
@@ -0,0 +1,24 @@
+import request from 'superagent';
+
+export default post;
+
+const post(api_token, room_id, msg) => {
+  return new Promise((resolve, reject) => {
+    request
+      .post(endpoint(room_id))
+      .set('X-ChatWorkToken', api_token)
+      .send(`body=${encodeURIComponent(msg)}`)
+      .end((err, res) => {
+        if (!err && res.statusCode == 200){
+          resolve(res);
+        } else {
+          reject(err);
+        }
+      })
+  })
+}
+
+const endpoint = (room_id) => {
+  return `https://api.chatwork.com/v2/rooms/${roomid}/messages`
+}
+
