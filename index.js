035327c8da87f5706cec867b53ca10f97ad5a5ac
Nate-River56 <hydrangea56@me.com>
2017-02-27 01:37:51 +0900

    Add module hub2cw

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var aws = require('aws-sdk');
+var hub2cw = require('./lib/hub2cw')
 
 exports.handler = function(event, context){
   console.log('Received event:', JSON.stringify(event, null, 2));
+  hub2cw.run(event, context);
   context.done();
 }
