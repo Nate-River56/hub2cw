39ab854df0a82f475cb57626f3e063cfe50bd524
Nate-River56 <hydrangea56@me.com>
2017-02-27 10:34:12 +0900

    Use Promise in superagent

--- src/postcw.js
+++ src/postcw.js
@@ -1,18 +1,22 @@
 import request from 'superagent';
 
 exports.message = (api_token, room_id, msg) => {
-  request
-    .post(endpoint(room_id))
-    .set('X-ChatWorkToken', api_token)
-    .send(`body=${encodeURIComponent(msg)}`)
-    .end((err, res) => {
-      if (!err && res.statusCode == 200){
-        console.log("Post Success!!");
-      } else {
-        console.log("Post Failed");
-        console.log(err);
-      }
-    })
+  return new Promise((resolve, reject) => {
+    request
+      .post(endpoint(room_id))
+      .set('X-ChatWorkToken', api_token)
+      .send(`body=${encodeURIComponent(msg)}`)
+      .end((err, res) => {
+        if (!err && res.statusCode == 200){
+          console.log("Post Success!!");
+          resolve(res);
+        } else {
+          console.log("Post Failed");
+          console.log(err);
+          reject(err);
+        }
+      })
+  })
 }
 
 const endpoint = (room_id) => {
