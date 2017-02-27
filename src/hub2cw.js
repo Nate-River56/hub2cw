3324830e4ee09f19b4068f5bf953b5e15cd32d3f
Nate-River56 <hydrangea56@me.com>
2017-02-27 14:00:58 +0900

    Added KMS encryption

--- src/hub2cw.js
+++ src/hub2cw.js
@@ -1,10 +1,18 @@
 
+import aws from 'aws-sdk';
 import hub from './hub_event.js';
 import cw from './postcw.js';
+
 
 exports.run = (event, context) => {
 
   let text = '';
+  
+  const enc_token = process.env.CHATWORK_API_TOKEN;
+  let dec_token;
+  
+  const enc_room_id = process.env.CHATWORK_ROOM_ID;
+  let dec_room_id;
 
   text = hub.getmsg(event);
 
@@ -15,11 +23,29 @@ exports.run = (event, context) => {
     return;
   }
 
-  cw.message(
-    process.env.CHATWORK_API_TOKEN,
-    process.env.CHATWORK_ROOM_ID,
-    text
-  );
 
+  const kms = new aws.KMS();
+
+  kms.decrypt({CiphertextBlob: new Buffer(enc_token, 'base64')},(err, data)=> {
+    if (err){
+      console.log('Decrypt error:', err);
+      return callback(err);
+    }
+    dec_token = data.Plaintext.toString('ascii');
+
+    kms.decrypt({CiphertextBlob: new Buffer(enc_room_id, 'base64')},(err, data)=> {
+      if (err){
+        console.log('Decrypt error:', err);
+        return callback(err);
+      }
+      dec_room_id = data.Plaintext.toString('ascii');
+
+      cw.message(
+        dec_token,
+        dec_room_id,
+        text
+      );
+    });
+  });
 }
 
