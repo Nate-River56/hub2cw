3ee81ddf7cc44475d871b2157f36111facf26446
Nate-River56 <hydrangea56@me.com>
2017-03-04 17:45:51 +0900

    Convert to Promise with hub_event

--- src/hub2cw.js
+++ src/hub2cw.js
@@ -4,26 +4,18 @@ import kms from './kms_descrypt.js';
 
 exports.run = (event, context) => {
 
-  let text = '';
-
   const enc_token = process.env.CHATWORK_API_TOKEN;
   const enc_room_id = process.env.CHATWORK_ROOM_ID;
 
-  text = hub.getmsg(event);
-
-  if(!text){
-    context.done();
-    return;
-  }
-
   Promise.all([
     kms.dec(enc_token),
-    kms.dec(enc_room_id)
+    kms.dec(enc_room_id),
+    hub.getmsg(event)
   ]).then((decVal)=> {
     cw.message(
       decVal[0], // Decrypted_token
       decVal[1], // Decrypted_room_id
-      text
+      decVal[2]  // text
     );
   }, (reason)=> {
     console.log(reason);
--- src/hub_event.js
+++ src/hub_event.js
@@ -1,132 +1,141 @@
 import builder from './builder.js';
 
 exports.getmsg = (event) => {
-  let eventName = event.Records[0].Sns.MessageAttributes['X-Github-Event'].Value;
-  console.log(eventName);
+  return new Promise((resolve, reject) => {
+    let eventName = event.Records[0].Sns.MessageAttributes['X-Github-Event'].Value;
+    console.log(eventName);
 
-  const payload = JSON.parse(event.Records[0].Sns.Message);
-  console.log(JSON.stringify(payload, null, 2));
+    const payload = JSON.parse(event.Records[0].Sns.Message);
+    console.log(JSON.stringify(payload, null, 2));
 
-  let text = '';
+    let text = '';
 
-  switch (eventName) {
-    // Any time a Commit is commented on.
-    case 'commit_comment':
-      // text += builder.commit_comment(payload);
-    break;
+    text += calltext(eventName, payload);
 
-    // Any time a Branch or Tag is created.
-    case 'create':
-      // text += builder.create(payload);
-    break;
+    if(!text){
+      reject("");
+    }
+    resolve(text);
 
-    // Any time a Branch or Tag is deleted.
-    case 'delete':
-      // text += builder.delete(payload);
-    break;
+  });
+}
 
-    // Any time a Repository is forked.
-    case 'fork':
-      // text += builder.fork(payload);
-    break;
+const calltext = (eventName, payload) => {
+    switch (eventName) {
+      // Any time a Commit is commented on.
+      case 'commit_comment':
+        // return builder.commit_comment(payload);
+      break;
 
-    // Any time a comment on an issue is created, edited, or deleted.
-    case 'issue_comment':
-      // text += builder.issue_comment(payload);
-    break;
+      // Any time a Branch or Tag is created.
+      case 'create':
+        // return builder.create(payload);
+      break;
 
-    // Any time an Issue is assigned, unassigned, labeled,
-    // unlabeled, opened, edited, milestoned, demilestoned,
-    // closed, or reopened.
-    case 'issues':
-      // text += builder.issue(payload);
-    break;
+      // Any time a Branch or Tag is deleted.
+      case 'delete':
+        // return builder.delete(payload);
+      break;
 
-    // Any time a Label is created, edited, or deleted.
-    case 'label':
-      // text += builder.label(payload);
-    break;
+      // Any time a Repository is forked.
+      case 'fork':
+        // return builder.fork(payload);
+      break;
 
-    // Any time a User is added or removed as a collaborator
-    // to a Repository, or has their permissions modified.
-    case 'member':
-      // text += builder.member(payload);
-    break;
+      // Any time a comment on an issue is created, edited, or deleted.
+      case 'issue_comment':
+        // return builder.issue_comment(payload);
+      break;
 
-    // Any time a User is added or removed from a team.
-    // Organization hooks only.
-    case 'membership':
-      // text += builder.membership(payload);
-    break;
+      // Any time an Issue is assigned, unassigned, labeled,
+      // unlabeled, opened, edited, milestoned, demilestoned,
+      // closed, or reopened.
+      case 'issues':
+        // return builder.issue(payload);
+      break;
 
-    // Any time a Milestone is created, closed, opened,
-    // edited, or deleted.
-    case 'milestone':
-      // text += builder.milestone(payload);
-    break;
+      // Any time a Label is created, edited, or deleted.
+      case 'label':
+        // return builder.label(payload);
+      break;
 
-    // Any time a user is added, removed, or invited
-    // to an Organization. Organization hooks only.
-    case 'organization':
-      // text += builder.organization(payload);
-    break;
+      // Any time a User is added or removed as a collaborator
+      // to a Repository, or has their permissions modified.
+      case 'member':
+        // return builder.member(payload);
+      break;
 
-    // Any time a Repository changes from private to public.
-    case 'public':
-      // text += builder.public(payload);
-    break;
+      // Any time a User is added or removed from a team.
+      // Organization hooks only.
+      case 'membership':
+        // return builder.membership(payload);
+      break;
 
-    // Any time a comment on a pull request's unified diff
-    // is created, edited, or deleted (in the Files Changed tab).
-    case 'pull_request_review_comment':
-      // text += builder.pull_request_review_comment(payload);
-    break;
+      // Any time a Milestone is created, closed, opened,
+      // edited, or deleted.
+      case 'milestone':
+        // return builder.milestone(payload);
+      break;
 
-    // Any time a pull request review is submitted.
-    case 'pull_request_review':
-      // text += builder.pull_request_review(payload);
-    break;
+      // Any time a user is added, removed, or invited
+      // to an Organization. Organization hooks only.
+      case 'organization':
+        // return builder.organization(payload);
+      break;
 
-    // Any time a pull request is assigned, unassigned,
-    // labeled, unlabeled, opened, edited, closed, reopened,
-    // or synchronized (updated due to a new push in the branch
-    // that the pull request is tracking). Also any time
-    // a pull request review is requested,
-    // or a review request is removed.
-    case 'pull_request':
-      // text += builder.pull_request(payload);
-    break;
+      // Any time a Repository changes from private to public.
+      case 'public':
+        // return builder.public(payload);
+      break;
 
-    // Any Git push to a Repository, including editing tags or
-    // branches. Commits via API actions that update references
-    // are also counted. This is the default event.
-    case 'push':
-      text += builder.push(payload);
-    break;
+      // Any time a comment on a pull request's unified diff
+      // is created, edited, or deleted (in the Files Changed tab).
+      case 'pull_request_review_comment':
+        // return builder.pull_request_review_comment(payload);
+      break;
 
-    // Any time a Repository is created, deleted
-    // (organization hooks only), made public, or made private.
-    case 'repository':
-      // text += builder.repository(payload);
-    break;
+      // Any time a pull request review is submitted.
+      case 'pull_request_review':
+        // return builder.pull_request_review(payload);
+      break;
 
-    // Any time a Release is published in a Repository.
-    case 'release':
-      // text += builder.relase(payload);
-    break;
+      // Any time a pull request is assigned, unassigned,
+      // labeled, unlabeled, opened, edited, closed, reopened,
+      // or synchronized (updated due to a new push in the branch
+      // that the pull request is tracking). Also any time
+      // a pull request review is requested,
+      // or a review request is removed.
+      case 'pull_request':
+        // return builder.pull_request(payload);
+      break;
 
-    // Any time a Repository has a status update from the API
-    case 'status':
-      // text += builder.status(payload);
-    break;
+      // Any Git push to a Repository, including editing tags or
+      // branches. Commits via API actions that update references
+      // are also counted. This is the default event.
+      case 'push':
+        return builder.push(payload);
+      break;
 
-    // Any time a User stars a Repository.
-    case 'watch':
-      // text += builder.watch(payload);
-    break;
-  }
+      // Any time a Repository is created, deleted
+      // (organization hooks only), made public, or made private.
+      case 'repository':
+        // return builder.repository(payload);
+      break;
 
-  console.log(text);
+      // Any time a Release is published in a Repository.
+      case 'release':
+        // return builder.relase(payload);
+      break;
 
-  return text;
+      // Any time a Repository has a status update from the API
+      case 'status':
+        // return builder.status(payload);
+      break;
+
+      // Any time a User stars a Repository.
+      case 'watch':
+        // return builder.watch(payload);
+      break;
+    }
+    return '';
 }
